0f1e4207d2d55dd21e37652a15fa891048b646b3
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-04-02 13:24:18 +0400

    added home page table and its collection views

---
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix
+//
+//  Created by Ramazan Abdullayev on 02.04.22.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 140, height: 200)
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return collection
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.backgroundColor = .systemOrange
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        collectionView.frame = contentView.bounds
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemYellow
+        
+        return cell
+    }
+}
---
+++ Netflix/Views/HeroHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  HeroHeaderView.swift
+//  Netflix
+//
+//  Created by Ramazan Abdullayev on 02.04.22.
+//
+
+import UIKit
+
+class HeroHeaderView: UIView {
+    
+    
+}

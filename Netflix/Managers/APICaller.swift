7b5a46b48edf34f3d6c8fe4e755731852e637a8b
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-04-03 03:11:36 +0400

    created new api caller base class

---
+++ Netflix/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  Netflix
+//
+//  Created by Ramazan Abdullayev on 03.04.22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "01ca7edc311279a7eed53a2cb6af6000"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+}

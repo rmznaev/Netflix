86260214bc4f994637779be5b40123fb52a31169
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-04-09 21:44:28 +0400

    added popular movies fetching method

--- Netflix/Managers/APICaller.swift
+++ Netflix/Managers/APICaller.swift
@@ -75,4 +75,25 @@ class APICaller {
         
         task.resume()
     }
+    
+    func getPopularMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data else {
+                return
+            }
+
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
 }

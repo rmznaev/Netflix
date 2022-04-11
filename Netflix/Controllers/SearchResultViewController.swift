3dcd03eaecdc6bfcbb609ed398e8c7916778ee90
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-04-11 23:46:20 +0400

    refactored file structure & added new Search result page

---
+++ Netflix/Controllers/SearchResultViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultViewController.swift
+//  Netflix
+//
+//  Created by Ramazan Abdullayev on 11.04.22.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+    
+}

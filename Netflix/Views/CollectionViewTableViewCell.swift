13e02e502771ef006c08d4ae21a434d740b0c640
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-04-16 17:18:21 +0400

    added context downloading feature for home page movies (long press)

--- Netflix/Views/CollectionViewTableViewCell.swift
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -56,6 +56,10 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath) {
+        print("Downloading \(String(describing: self.titles[indexPath.row].original_title))")
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -94,5 +98,31 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: nil) { [weak self] _ in
+                let downloadAction = UIAction(
+                    title: "Download",
+                    subtitle: nil,
+                    image: nil,
+                    identifier: nil,
+                    discoverabilityTitle: nil,
+                    state: .off) { _ in
+                        self?.downloadTitleAt(indexPath: indexPath)
+                    }
+                
+                return UIMenu(
+                    title: "",
+                    image: nil,
+                    identifier: nil,
+                    options: .displayInline,
+                    children: [downloadAction]
+                )
+            }
+        
+        return config
     }
 }
